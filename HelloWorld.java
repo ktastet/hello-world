472cbc88c753e6317c9a819c6b1d83c54f3fe926
Kevin Tastet <kevin.tastet@pcc.edu>
2018-10-22 13:42:15 -0700

    add main method printing hello world

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,4 +1,10 @@
 package edu.pcc.cis.helloworld;
 
+/**
+ * Created by Kevin Tastet and Cara Tang on 22 October.
+ */
 public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
 }
